b48acaef238fa963abe25af5ab5b54ec6501c89d
mathmerizing <mathmerizing@gmail.com>
2020-03-26 23:03:10 +0100

    In deal.ii dataset started working on renumbering the DoFs to reduce the bandwidth

--- DealScripts/main.cc
+++ DealScripts/main.cc
@@ -15,6 +15,7 @@
 #include <deal.II/lac/affine_constraints.h>
 #include <deal.II/lac/full_matrix.h>
 #include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
 
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_accessor.h>
@@ -23,6 +24,7 @@
 
 #include <deal.II/dofs/dof_accessor.h>
 #include <deal.II/dofs/dof_tools.h>
+#include <deal.II/dofs/dof_renumbering.h>
 
 #include <deal.II/fe/fe_q.h>
 #include <deal.II/fe/fe_values.h>
@@ -33,17 +35,21 @@
 
 using namespace dealii;
 
+// enum for type of DoF renumbering
+enum Renumbering { cuthill, king };
+
 namespace FEMatrixGenerator
 {
     template <int dim>
     class FEProblem
     {
     public:
-        FEProblem(const unsigned int degree, double first_coeff, double second_coeff);
-        void run();
+        FEProblem(const unsigned int degree, double first_coeff, double second_coeff, bool renumber);
+        void run(Renumbering renum);
 
     private:
         void setup_system();
+        void renumbering(Renumbering renum);
         void assemble_system();
         void invert_system();
         void output_matrices();
@@ -53,7 +59,8 @@ namespace FEMatrixGenerator
         DoFHandler<dim>     dof_handler;
 
         SparsityPattern         sparsity_pattern;
-        SparseMatrix<double>    system_matrix;
+        // SparseMatrix<double>    system_matrix;
+        FullMatrix<double>      system_matrix;
 
         AffineConstraints<double> constraints;
 
@@ -74,14 +81,113 @@ namespace FEMatrixGenerator
         , second_coeff(second_coeff)
         , reduce_bandwidth(renumber)
     {}
+
+    template <int dim>
+    void FEProblem<dim>::setup_system()
+    {
+        dof_handler.distribute_dofs(fe);
+
+        DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(),
+                                                        dof_handler.n_dofs());
+        DoFTools::make_sparsity_pattern(dof_handler, dynamic_sparsity_pattern);
+        std::cout << "Unordered matrix   - bandwidth: " << dynamic_sparsity_pattern.bandwidth() << std::endl;
+        
+        sparsity_pattern.copy_from(dynamic_sparsity_pattern);
+        std::ofstream out("sparsity_pattern_unordered.svg");
+        sparsity_pattern.print_svg(out);
+    }
+
+    template <int dim>
+    void FEProblem<dim>::renumbering(Renumbering renum)
+    {
+        // chose DoF renumbering
+        switch (renum)
+        {
+        case Renumbering::cuthill:
+            DoFRenumbering::Cuthill_McKee(
+                dof_handler,    // dof_handler
+                false,          // reversed_numbering
+                false           // use_constraints
+            );
+            break;
+        case Renumbering::king:
+            DoFRenumbering::boost::king_ordering(
+                dof_handler,    // dof_handler
+                false,          // reversed_numbering
+                false           // use_constraints
+            );
+            break;
+        }
+
+        DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs(),
+                                                        dof_handler.n_dofs());
+        DoFTools::make_sparsity_pattern(dof_handler, dynamic_sparsity_pattern);
+        std::cout << "Renumbered matrix  - bandwidth: " << dynamic_sparsity_pattern.bandwidth() << std::endl;
+
+        sparsity_pattern.copy_from(dynamic_sparsity_pattern);
+        std::ofstream out("sparsity_pattern_cuthill.svg");
+        sparsity_pattern.print_svg(out);
+    }
+
+    template <int dim>
+    void FEProblem<dim>::assemble_system()
+    {
+        // TO DO
+    }
+
+    template <int dim>
+    void FEProblem<dim>::invert_system()
+    {
+        // TO DO
+    }
+
+    template <int dim>
+    void FEProblem<dim>::output_matrices()
+    {
+        // TO DO
+    }
+
+    template <int dim>
+    void FEProblem<dim>::run(Renumbering renum)
+    {
+        // create grid
+        int global_refinements = 3;
+        GridGenerator::hyper_ball(triangulation);
+        triangulation.refine_global(global_refinements);
+
+        setup_system();
+        renumbering(renum);
+        assemble_system();
+        invert_system();
+        output_matrices();
+    }
+
 } // namespace FEMatrixGenerator
 
 int main()
 {
-    // TO DO: read coefficients from options.prm with ParameterHandler
-    double coeff_1 = 1.0;
-    double coeff_2 = 0.1;
-    const unsigned int degree = 1;
-    bool reduce_bandwidth = true;
-    FEMatrixGenerator::FEProblem<2> problem(degree, coeff_1, coeff_2, reduce_bandwidth);
+    try
+    {
+        // TO DO: read coefficients from options.prm with ParameterHandler
+        double coeff_1 = 1.0;
+        double coeff_2 = 0.1;
+        const unsigned int degree = 2;
+        bool reduce_bandwidth = true;
+        Renumbering renum = Renumbering::king;
+        
+        FEMatrixGenerator::FEProblem<2> problem(degree, coeff_1, coeff_2, reduce_bandwidth);
+        problem.run(renum);
+    }
+    catch (std::exception &exc)
+    {
+        std::cerr << std::endl << std::endl
+                  << "-----------------------------------"
+                  << std::endl;
+        std::cerr << "Exception on processing: " << std::endl
+                  << exc.what() << std::endl
+                  << "Aborting!"<< std::endl
+                  << "-----------------------------------"
+                  << std::endl;
+    }
 }
+    
